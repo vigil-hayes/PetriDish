c03839285aea7e13867cf76499a66c631caf943e
Morgan Vigil <mvigil@cs.ucsb.edu>
2015-08-10 13:33:46 -0700

    Fixing instructions for Space.h

--- Space.h
+++ Space.h
@@ -1,7 +1,7 @@
 // TODO Step 8: Implement Space
 // 1. Space is a struct that represents a unit of physical space in a PetriDish
-// 2. Space has a Bacteria that currently occupies its space
-// 3. Space has a pointer to another Space called North
-// 4. Space has a pointer to another Space called South
-// 5. When spaces are connected together, they can form a chain of doubly linked North-to-South Spaces
+// 2. Space has a Bacteria that currently occupies its space called bacteria
+// 3. Space has a pointer to another Space called north
+// 4. Space has a pointer to another Space called south
+// 5. When Spaces are connected together, they can form a chain of doubly linked North-to-South Spaces
 
