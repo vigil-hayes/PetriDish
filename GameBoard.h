5f90253ef5d951a623175e5e2dd579d667659a47
Morgan Vigil <mvigil@cs.ucsb.edu>
2015-08-10 13:43:27 -0700

    Adding a header file for GameBoard struct

---
+++ GameBoard.h
@@ -0,0 +1,4 @@
+// TODO Step 9: Create a structure called GameBoard
+// 1. GameBoard has a member variable called height
+// 2. GameBoard has a member variable called width
+// 3. GameBoard has an member variable that is an array of doubly linked list of Spaces called board
