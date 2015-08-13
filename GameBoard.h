63c44692bac8893ebd42708c199f9f6eec78b84d
Morgan Vigil <mvigil@cs.ucsb.edu>
2015-08-13 10:55:47 -0700

    Adding structure definitions to the code

--- GameBoard.h
+++ GameBoard.h
@@ -2,3 +2,8 @@
 // 1. GameBoard has a member variable called height
 // 2. GameBoard has a member variable called width
 // 3. GameBoard has an member variable that is an array of doubly linked list of Spaces called board
+struct GameBoard {
+	int height;
+	int width;
+	Spaces * board[]; // Dynamic array of doubly linked list 
+};
--- Space.h
+++ Space.h
@@ -4,4 +4,8 @@
 // 3. Space has a pointer to another Space called north
 // 4. Space has a pointer to another Space called south
 // 5. When Spaces are connected together, they can form a chain of doubly linked North-to-South Spaces
-
+struct Space {
+	Bacteria bacteria;
+	Space * north;
+	Space * south;
+};
